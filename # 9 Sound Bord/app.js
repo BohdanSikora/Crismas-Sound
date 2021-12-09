29f81b6a42152e63c5aa440b172515ca1f095298
Sikora Bohdan <91610850+BohdanSikora@users.noreply.github.com>
2021-12-09 20:36:25 +0200

    Add files via upload
    New

---
+++ # 9 Sound Bord/app.js
@@ -0,0 +1,11 @@
+const sounds = ['crismas','instrumental', 'sia']
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
+})
